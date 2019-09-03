a3ab91d9b1e5f5be5133ed8a1d9b317c8f6c5ee2
Lucas7x <lucassx1995@gmail.com>
2019-09-03 10:30:34 -0300

    adicionando metodo de login em Usuario

--- DAO/index.php
+++ DAO/index.php
@@ -8,7 +8,21 @@
   echo $usuario;
   */
 
+  /*
   //carrega uma lista de usuarios
   $lista = Usuario::getList();
   echo json_encode($lista);
+  */
+
+  /*
+  //carrega usuarios buscando pelo login
+  $search = Usuario::search("l");
+  echo json_encode($search);
+  */
+
+  //carrega um usuario usando o login e setDessenha
+  $usuario = new Usuario();
+  $usuario->login("roota","qwerty");
+  echo $usuario;
+
 ?>
