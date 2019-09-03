237eb9b1539ece713a6f765e76236ccf54462c25
Lucas7x <lucassx1995@gmail.com>
2019-09-03 09:46:30 -0300

    adicionado o metodo getList

--- DAO/index.php
+++ DAO/index.php
@@ -1,8 +1,14 @@
 <?php
   require_once("config.php");
 
+  /*
+  //carrega um usuario
   $usuario = new Usuario();
   $usuario->loadById(1);
   echo $usuario;
+  */
 
+  //carrega uma lista de usuarios
+  $lista = Usuario::getList();
+  echo json_encode($lista);
 ?>
